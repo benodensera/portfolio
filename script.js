472499b3cf611a0d5b2a99966025e05e52bd5c02
DevTech-Agency <devtechagency.ec@gmail.com>
2026-04-01 21:13:23 -0500

    stats section & counter animation

--- script.js
+++ script.js
@@ -56,3 +56,40 @@ filters.forEach(btn => {
 
   });
 });
+
+
+// counter animation function
+
+function animateCounter(counter) {
+  const target = +counter.getAttribute("data-target");
+  const hasPlus = counter.dataset.plus === "true";
+  let count = 0;
+
+  const updateCounter = () => {
+    const increment = target / 100;
+
+    if (count < target) {
+      count += increment;
+      counter.innerText = Math.ceil(count);
+      setTimeout(updateCounter, 20);
+    } else {
+      counter.innerText = hasPlus ? target + "+" : target;
+    }
+  };
+
+  updateCounter();
+}
+
+const counters = document.querySelectorAll(".counter");
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateCounter(entry.target);
+      observer.unobserve(entry.target); // run once
+    }
+  });
+}, { threshold: 1 });
+
+counters.forEach(counter => {
+  observer.observe(counter);
+});
