b86c736d6bed51ac9987da6c67a5fb6f65e7b8c4
DevTech-Agency <devtechagency.ec@gmail.com>
2026-05-12 20:58:59 -0500

    floating navigation + js

--- script.js
+++ script.js
@@ -384,3 +384,66 @@ btn.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
+
+
+// floating navigation
+
+const floatingNav = document.getElementById("floatingNav");
+const floatingExpandBtn = document.getElementById("floatingExpandBtn");
+
+// expand
+
+floatingExpandBtn.addEventListener("mouseenter", () => {
+  floatingNav.classList.add("expanded");
+});
+
+// collapse
+
+floatingNav.addEventListener("mouseleave", () => {
+  floatingNav.classList.remove("expanded");
+});
+
+// floating nav active section
+
+const floatingLinks = document.querySelectorAll(".floating-nav a");
+const sections = document.querySelectorAll("section[id], footer[id]");
+
+// click active
+
+floatingLinks.forEach(link => {
+  link.addEventListener("click", () => {
+
+    floatingLinks.forEach(item => {
+      item.classList.remove("active");
+    });
+
+    link.classList.add("active");
+  });
+});
+
+// scroll active
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (window.scrollY >= sectionTop - 300) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  floatingLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
